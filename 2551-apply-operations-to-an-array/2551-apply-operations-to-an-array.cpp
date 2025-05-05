4a7fae40fc136c26bc51e086d7efa83aeb24b407
Charishma <charishma.coder@gmail.com>
2025-05-06 00:17:33 +0530

    Time: 0 ms (100%), Space: 13.1 MB (61.54%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for (int i=0;i<nums.size()-1;i++){
+            if (nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        stable_partition(nums.begin(), nums.end(), [](int x) {
+            return x != 0;
+        });
+        return nums;
+    }
+};
